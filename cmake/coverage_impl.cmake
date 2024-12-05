5c9da0dbad6c6a2e6c472d7e6e6d594d5f3e1040
Aleksey Loginov <victimsnino@gmail.com>
2024-12-05 22:40:47 +0300

    fix coverage

--- cmake/coverage_impl.cmake
+++ cmake/coverage_impl.cmake
@@ -11,14 +11,13 @@ endforeach()
 set(COVERAGE_ADDITIONAL_ARGS -instr-profile=${BINARY_DIR}/coverage.profdata
                              ${COVERAGE_OBJECTS_RES})
 
+execute_process(COMMAND llvm-profdata merge -sparse=true ${COVERAGE_PROFRAW_FILES} -o ${BINARY_DIR}/coverage.profdata)
+
 execute_process(
-  COMMAND llvm-profdata merge -sparse=true ${COVERAGE_PROFRAW_FILES} -o
-          ${BINARY_DIR}/coverage.profdata
   COMMAND
     llvm-cov show --ignore-filename-regex=build --show-branches=count
     --show-expansions --show-line-counts --show-line-counts-or-regions
     --show-regions ${COVERAGE_ADDITIONAL_ARGS}
-  WORKING_DIRECTORY ${BINARY_DIR}
   OUTPUT_VARIABLE RES
   OUTPUT_STRIP_TRAILING_WHITESPACE)
 
