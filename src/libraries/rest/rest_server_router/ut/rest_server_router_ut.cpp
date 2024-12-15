c03189a66476723ea3590cff5dc67807b856ce50
Aleksey Loginov <victimsnino@gmail.com>
2024-12-15 20:42:30 +0300

    cover router

--- src/libraries/rest/rest_server_router/ut/rest_server_router_ut.cpp
+++ src/libraries/rest/rest_server_router/ut/rest_server_router_ut.cpp
@@ -2,18 +2,56 @@
 
 #include <libraries/rest/rest_server_router/rest_server_router.hpp>
 
+auto CreateRequest(boost::beast::http::verb method, std::string path)
+{
+    auto req = boost::beast::http::request<boost::beast::http::string_body>{};
+    req.target(path);
+    req.method(method);
+    return req;
+}
 
 TEST_CASE("Router provide correct routing")
 {
     rest::Router router{};
-    SUBCASE("route empty")
+    SUBCASE("get empty")
     {
-        REQUIRE(router.Route(boost::beast::http::request<boost::beast::http::string_body>{}).result() == boost::beast::http::status::not_found);
+        REQUIRE(router.Route(CreateRequest(boost::beast::http::verb::get, {})).result() == boost::beast::http::status::not_found);
     }
-    SUBCASE("route unkown")
+
+    SUBCASE("get /test")
     {
-        auto req = boost::beast::http::request<boost::beast::http::string_body>{};
-        req.target("/unknown");
-        REQUIRE(router.Route(req).result() == boost::beast::http::status::not_found);
+        REQUIRE(router.Route(CreateRequest(boost::beast::http::verb::get, "/test")).result() == boost::beast::http::status::not_found);
+    }
+
+    SUBCASE("add get /test")
+    {
+        router.AddRoute("/test", boost::beast::http::verb::get, [](const auto&, const auto&) { return boost::beast::http::response<boost::beast::http::string_body>{}; });
+        SUBCASE("get /test")
+        {
+            REQUIRE(router.Route(CreateRequest(boost::beast::http::verb::get, "/test")).result() == boost::beast::http::status::ok);
+        }
+
+        SUBCASE("get /test_2")
+        {
+            REQUIRE(router.Route(CreateRequest(boost::beast::http::verb::get, "/test_2")).result() == boost::beast::http::status::not_found);
+        }
+
+        SUBCASE("post /test")
+        {
+            REQUIRE(router.Route(CreateRequest(boost::beast::http::verb::post, "/test")).result() == boost::beast::http::status::method_not_allowed);
+        }
+        SUBCASE("add post /test")
+        {
+            router.AddRoute("/test", boost::beast::http::verb::post, [](const auto& req, const auto&) { return boost::beast::http::response<boost::beast::http::string_body>{boost::beast::http::status::processing, req.version()}; });
+            SUBCASE("get /test")
+            {
+                REQUIRE(router.Route(CreateRequest(boost::beast::http::verb::get, "/test")).result() == boost::beast::http::status::ok);
+            }
+
+            SUBCASE("post /test")
+            {
+                REQUIRE(router.Route(CreateRequest(boost::beast::http::verb::post, "/test")).result() == boost::beast::http::status::processing);
+            }
+        }
     }
 }
